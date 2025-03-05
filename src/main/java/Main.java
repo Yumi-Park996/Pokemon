f43211d2b3f03f0482f6c5a57a71d5f030ace1b3
qus0in <qus0in@gmail.com>
2025-03-05 16:18:39 +0900

    Object Mapper

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,3 +1,7 @@
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties; // JsonIgnoreProperties 추가
+
 import java.io.IOException;
 import java.net.URI;
 import java.net.http.HttpClient;
@@ -10,8 +14,27 @@ public class Main {
         String apiUrl = "https://pokeapi.co/api/v2/pokemon/%d";
         HttpClient client = HttpClient.newHttpClient();
         Random rand = new Random();
-        HttpRequest request = HttpRequest.newBuilder().uri(URI.create(apiUrl.formatted(rand.nextInt(1,152)))).GET().build();
+        int pokemonId = rand.nextInt(1, 152);
+        HttpRequest request = HttpRequest.newBuilder().uri(URI.create(apiUrl.formatted(pokemonId))).GET().build();
         HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
-        System.out.println(response.body());
+
+        ObjectMapper mapper = new ObjectMapper();
+        Pokemon pokemon = mapper.readValue(response.body(), Pokemon.class);
+        if (pokemon.sprites != null && pokemon.sprites.frontDefault != null) {
+            System.out.println(pokemon.sprites.frontDefault);
+        } else {
+            System.out.println("포켓몬 이미지를 찾을 수 없습니다. (ID: " + pokemonId + ")");
+        }
     }
+}
+
+@JsonIgnoreProperties(ignoreUnknown = true) // 추가
+class Pokemon {
+    @JsonIgnoreProperties(ignoreUnknown = true) // 추가
+    public static class Sprites {
+        @JsonProperty("front_default")
+        public String frontDefault;
+    }
+
+    public Sprites sprites;
 }
